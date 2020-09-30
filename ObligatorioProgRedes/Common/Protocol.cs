0b73839e22ca5a7fc71425bbeaae9d2535b87479
lTato <andrestomas86@gmail.com>
2020-09-30 18:04:23 -0300

    Inicializacion del proyecto

---
+++ ObligatorioProgRedes/Common/Protocol.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Cache;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class Protocol
+    {
+
+        public static string Request = "REQ";
+
+        public static string Response = "RES";
+
+        public static int CommandLength = 2;
+
+        public static int DataLength = 4;
+
+    }
+}
---
+++ ObligatorioProgRedes/Server/Server.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Server
+    {
+        private static Socket _server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        private static IPAddress address = IPAddress.Parse("127.0.0.1");
+
+        private static int port = 6000;
+
+        private const int BUFFER_SIZE = 1024;
+
+        private const int CONNECTIONS = 10;
+
+        private static bool isServerUp = false;
+
+
+
+        public void StartServer()
+
+        {
+
+            IPEndPoint endPoint = new IPEndPoint(address, port);
+
+            isServerUp = true;
+
+            _server.Bind(endPoint);
+
+            _server.Listen(CONNECTIONS);
+
+            Console.WriteLine("Listening.....");
+
+            while (isServerUp)
+
+            {
+
+                Socket _clientSocket = _server.Accept();
+
+                new Thread(() => HandleClient(_clientSocket)).Start();
+
+            }
+
+        }
+
+        public void HandleClient(Socket socket)
+        {
+         
+
+        }
+
+    }
+}
