0405d1c8c2a60b927bfc0784e3f196d40efbee06
Diego <d.baccino@outlook.com>
2020-10-23 14:43:29 -0300

    Log in y SignUp en server

--- ObligatorioProgRedes/Server/Server.cs
+++ ObligatorioProgRedes/Server/Server.cs
@@ -1,5 +1,6 @@
 ﻿using Common;
 using Domain;
+using Exceptions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,10 +27,10 @@ namespace Server
 
         private static bool isServerUp = false;
 
-        List<User> UsersLogged = new List<User>();
+        private static List<User> Users = new List<User>() { new User() { Username = "pepito", Password="pepeito",Photos=new List<Photo>()}};
 
-        List<User> Users = new List<User>();
-
+        private static List<User> UsersLogged = new List<User>();
+        
 
         public void StartServer()
 
@@ -56,7 +57,7 @@ namespace Server
 
         public void HandleClient(Socket socket)
         {
-           
+            User user;
             while (true)
             {
                 int command;
@@ -69,6 +70,7 @@ namespace Server
                 while (received < headerLength)
                 {
                     received += socket.Receive(header, received, headerLength - received, SocketFlags.None);
+                    
                 }
                 
                 Header header2 = new Header(header);
@@ -82,22 +84,105 @@ namespace Server
                 {
                     received += socket.Receive(data, received, dataLength - received, SocketFlags.None);
                 }
+
                 var word = Encoding.UTF8.GetString(data);
+
                 switch (command)
                 {
-                    case 1:
-                        Login();
-                        break;
                     case 2:
-                        SignUp();
+                        try
+                        {
+                            user = Login(word);
+                            break;
+                        }
+                        catch (Exception)
+                        {
+                            break;
+                        }
+                    case 1:
+                        SignUp(word);
                         break;
                     default:
-                        Console.WriteLine("Default case");
+                        Console.WriteLine("Invalid command");
                         break;
                 }
             }
 
         }
 
+        private static void GetCredentials(string word, out string username, out string password)
+        {
+            var credentials = word.Split('%');
+            username = credentials[0];
+            password = credentials[1];
+        }
+
+        private static User Login(string credentials)
+        {
+            string username, password;
+            GetCredentials(credentials, out username, out password);
+            User user = GetUserByCredentials(username, password);
+            bool exist = false;
+            lock (Users)
+            {
+                exist = Users.Contains(user);
+            }
+            //No me pueden borrar el user de la lista?
+            if (exist)
+            {
+                lock (UsersLogged)
+                {
+                    if (!UsersLogged.Contains(user))
+                    {
+                        UsersLogged.Add(user);
+                        return user;
+                    }
+                    else
+                        throw new IsLoggedException();
+                }
+            }
+            else
+                throw new UserNotExistException();
+        }
+
+        private static User GetUserByCredentials(string username, string password)
+        {
+            lock (Users)
+            {
+                return Users.Where
+                    (x => x.Username.Equals(username) && x.Password.Equals(password)).FirstOrDefault();
+            }
+        }
+
+        private static bool IsLogged(User user)
+        {
+            return UsersLogged.Contains(user);
+        }
+        
+        private static User CreateUser(string username, string password)
+        {
+            User user = new User();
+            user.Username = username;
+            user.Password = password;
+            user.Photos = new List<Photo>();
+            return user;
+        }
+
+        private static void SignUp(string credentials)
+        {
+            string username, password;
+            GetCredentials(credentials, out username, out password);
+
+            User user = CreateUser(username, password);
+            lock (Users)
+            {
+                if (!Users.Contains(user))
+                {
+                    Users.Add(user);
+                }
+                else
+                    throw new UserAlreadyExistException();
+            }
+        }
     }
 }
